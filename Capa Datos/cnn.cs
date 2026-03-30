d0adc748a92698f59f6ccc226c61e2c1a840a31f
Daniel A <martinezapata809@gmail.com>
2026-03-30 16:51:08 -0400

    Creacion del repositorio de Usuarios

---
+++ Capa Datos/cnn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos
+{
+    internal class cnn
+    {
+        public static string db = @"Data Source=DANIEL;Initial Catalog=RegistroUsuarios;Integrated Security=True";
+    }
+}
