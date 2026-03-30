2ec0343e7d8753cd2cda6484ca68f6fcae5451ea
Daniel A <martinezapata809@gmail.com>
2026-03-30 17:34:06 -0400

    Logica de negocio de la tabla usuarios

--- Capa Datos/cnn.cs
+++ Capa Datos/cnn.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Capa_Datos
 {
-    internal class cnn
+    public class cnn
     {
         public static string db = @"Data Source=DANIEL;Initial Catalog=RegistroUsuarios;Integrated Security=True";
     }
---
+++ Logica Negocios/Negocio_Usuarios.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Datos;
+
+namespace Logica_Negocios
+{
+    public class Negocio_Usuarios
+    {
+        Capa_Datos.UsuariosRepository data_usuario = new UsuariosRepository();
+
+        public void AgregarUsuario(string _nombre, string _contraseña)
+        {
+            data_usuario.AgregarUsuario(_nombre, _contraseña);
+        }
+
+        public List<Capa_Datos.UsuariosRepository.Usuario> ObtenerUsuarios(string _nombre = "")
+        {
+            return data_usuario.ObtenerUsuarios(_nombre);
+        }
+
+        public Capa_Datos.UsuariosRepository.Usuario ObtenerUsuarioId(int _id)
+        {
+            return data_usuario.BuscarUsuario(_id);
+        }
+
+        public void ActualizarUsuario(int _id, string _nombre, string _contraseña)
+        {
+            data_usuario.ActualizarUsuario(_id, _nombre, _contraseña);
+        }
+
+        public void EliminarUsuario(int _id)
+        {
+            data_usuario.EliminarUsuario(_id);
+        }
+    }
+}
